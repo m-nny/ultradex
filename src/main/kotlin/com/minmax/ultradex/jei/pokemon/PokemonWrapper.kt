d71503a76c1fe3c4af5c887f82ab816b87d4bcc1
Alibek Manabayev <alibek.manabayev@gmail.com>
2023-05-15 02:35:36 +0100

    migrate UltraDex, PixelmonJEIPlugin, PokemonCategory

--- src/main/kotlin/com/minmax/ultradex/jei/pokemon/PokemonWrapper.kt
+++ src/main/kotlin/com/minmax/ultradex/jei/pokemon/PokemonWrapper.kt
@@ -3,27 +3,15 @@ package com.minmax.ultradex.jei.pokemon
 import com.minmax.ultradex.config.Settings
 import com.minmax.ultradex.pokemon.PokemonSpawnHelper.getBiomes
 import com.mojang.blaze3d.matrix.MatrixStack
-import com.pixelmonmod.pixelmon.api.pokemon.Pokemon
 import com.pixelmonmod.pixelmon.api.pokemon.drops.ItemWithChance
 import com.pixelmonmod.pixelmon.api.pokemon.drops.PokemonDropInformation
 import com.pixelmonmod.pixelmon.api.util.helpers.PokemonHelper
-import jeresources.compatibility.CompatBase
 import jeresources.util.Font
 import jeresources.util.RenderHelper
 import mezz.jei.api.constants.VanillaTypes
 import mezz.jei.api.gui.ingredient.ITooltipCallback
 import mezz.jei.api.ingredients.IIngredients
 import mezz.jei.api.recipe.category.extensions.IRecipeCategoryExtension
-import net.minecraft.entity.LivingEntity
-import net.minecraft.entity.boss.WitherEntity
-import net.minecraft.entity.boss.dragon.EnderDragonEntity
-import net.minecraft.entity.merchant.villager.VillagerEntity
-import net.minecraft.entity.merchant.villager.WanderingTraderEntity
-import net.minecraft.entity.monster.*
-import net.minecraft.entity.passive.AnimalEntity
-import net.minecraft.entity.passive.GolemEntity
-import net.minecraft.entity.passive.SquidEntity
-import net.minecraft.entity.passive.TurtleEntity
 import net.minecraft.item.ItemStack
 import net.minecraft.util.text.ITextComponent
 import net.minecraft.util.text.StringTextComponent
@@ -32,40 +20,30 @@ import javax.annotation.Nonnull
 
 class PokemonWrapper(pokemonDropInformation: PokemonDropInformation) : IRecipeCategoryExtension,
     ITooltipCallback<ItemStack> {
-    private val pokemon: Pokemon
-    private val livingEntity: LivingEntity
-    private var scale: Float
-    private val biomes: List<String>
+    private val pokemon = PokemonUtils.createOrdinaryPokemon(pokemonDropInformation.pokemonSpec)
+    private val livingEntity = pokemon.getOrCreatePixelmon();
+    private var scale = PokemonUtils.getScale(livingEntity)
+    private var offsetY = PokemonUtils.getOffsetY(livingEntity)
+    private val biomes = getBiomes(pokemon)
 
-    @JvmField
-    val drops: List<ItemWithChance>
-    private var offsetY: Int
+    val drops: List<ItemWithChance> = pokemonDropInformation.drops
 
-    init {
-        pokemon = pokemonDropInformation.pokemonSpec.create()
-        biomes = getBiomes(pokemon)
-        drops = pokemonDropInformation.drops
-        livingEntity = pokemonDropInformation.pokemonSpec.create(CompatBase.getWorld())
-        scale = getScale(livingEntity)
-        offsetY = getOffsetY(livingEntity)
-    }
-
-    override fun setIngredients(@Nonnull ingredients: IIngredients) {
-        ingredients.setOutputs(VanillaTypes.ITEM, drops.stream().map { obj: ItemWithChance -> obj.itemStack }
-            .collect(Collectors.toList()))
+    override fun setIngredients(ingredients: IIngredients) {
+        ingredients.setOutputs(VanillaTypes.ITEM,
+            drops.stream()
+                .map { obj -> obj.itemStack }
+                .collect(Collectors.toList())
+        )
     }
 
     override fun drawInfo(
         recipeWidth: Int,
         recipeHeight: Int,
-        @Nonnull matrixStack: MatrixStack,
+        matrixStack: MatrixStack,
         mouseX: Double,
         mouseY: Double
     ) {
-        val livingEntity = livingEntity
         RenderHelper.scissor(matrixStack, 7, 43, 59, 79)
-        scale = getScale(this.livingEntity)
-        offsetY = getOffsetY(this.livingEntity)
         RenderHelper.renderEntity(
             matrixStack,
             37, 105 - offsetY, scale.toDouble(),
@@ -95,66 +73,22 @@ class PokemonWrapper(pokemonDropInformation: PokemonDropInformation) : IRecipeCa
     override fun onTooltip(
         slotIndex: Int,
         input: Boolean,
-        @Nonnull ingredient: ItemStack,
-        @Nonnull tooltip: MutableList<ITextComponent>
+        ingredient: ItemStack,
+        tooltip: MutableList<ITextComponent>
     ) {
-        val list = getToolTip(ingredient)
-        if (list != null) tooltip.addAll(list)
+        tooltip.addAll(getToolTip(ingredient))
     }
 
-    private fun getToolTip(stack: ItemStack): List<ITextComponent>? {
+    private fun getToolTip(stack: ItemStack): List<ITextComponent> {
         for (item in drops) {
-            if (stack.sameItem(item.item)) return getTooltipText(item)
+            if (stack.sameItem(item.item)) {
+                return PokemonUtils.getTooltipText(item)
+            }
         }
-        return null
+        return emptyList()
     }
 
     private fun isOnBiome(mouseX: Double, mouseY: Double): Boolean {
         return 2 <= mouseX && mouseX < 165 && 22 <= mouseY && mouseY < 22 + 10
-    }
-
-    companion object {
-        private fun getScale(@Nonnull livingEntity: LivingEntity): Float {
-            val width = livingEntity.bbWidth
-            val height = livingEntity.bbHeight
-            return if (width <= height) {
-                if (height < 0.9) 50.0f else if (height < 1) 35.0f else if (height < 1.8) 33.0f else if (height < 2) 32.0f else if (height < 3) 24.0f else if (height < 4) 20.0f else 10.0f
-            } else {
-                if (width < 1) 38.0f else if (width < 2) 27.0f else if (width < 3) 13.0f else 9.0f
-            }
-        }
-
-        private fun getOffsetY(@Nonnull livingEntity: LivingEntity): Int {
-            var offsetY = 0
-            when (livingEntity) {
-                is SquidEntity -> offsetY = 20
-                is TurtleEntity -> offsetY = 10
-                is WitchEntity -> offsetY = -5
-                is GhastEntity -> offsetY = 15
-                is WitherEntity -> offsetY = -15
-                is EnderDragonEntity -> offsetY = 15
-                is EndermanEntity -> offsetY = -10
-                is GolemEntity -> offsetY = -10
-                is AnimalEntity -> offsetY = -20
-                is VillagerEntity -> offsetY = -15
-                is WanderingTraderEntity -> offsetY = -15
-                is BlazeEntity -> offsetY = -10
-                is CreeperEntity -> offsetY = -15
-            }
-            return offsetY
-        }
-
-        private fun getTooltipText(item: ItemWithChance): List<ITextComponent> {
-            return listOf<ITextComponent>(StringTextComponent(formatChance(item.chance) + " %"))
-        }
-
-        private fun formatChance(chance: Double): String {
-            val chance100 = chance * 100.0
-            return if (chance100 < 10) {
-                String.format("%.1f", chance100)
-            } else {
-                String.format("%2d", chance100.toInt())
-            }
-        }
     }
 }
