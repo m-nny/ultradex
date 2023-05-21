2dcb28eeb1d1798fe0ae863ad71dacd6eeca398e
Alibek Manabayev <alibek.manabayev@gmail.com>
2023-05-21 02:05:11 +0100

    find biome

--- src/main/kotlin/com/minmax/ultradex/client/ClientChatListener.kt
+++ src/main/kotlin/com/minmax/ultradex/client/ClientChatListener.kt
@@ -4,6 +4,7 @@ import com.minmax.ultradex.UltraDex
 import com.mojang.brigadier.exceptions.CommandSyntaxException
 import net.minecraft.client.Minecraft
 import net.minecraft.command.CommandSource
+import net.minecraft.util.text.TranslationTextComponent
 import net.minecraftforge.client.event.ClientChatEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 
@@ -13,15 +14,18 @@ object ClientChatListener {
         if (!event.message.startsWith("!")) {
             return
         }
+        UltraDex.LOGGER.debug("onPlayerChat(${event.message})")
+        val source = getCommandSource() ?: return
         try {
-            UltraDex.LOGGER.debug("onPlayerChat(${event.message})")
-            val source = getCommandSource() ?: return
             val parse = UltraDex.COMMANDS_DISPATCHER.parse(event.message.substring(1), source)
             if (parse.context.nodes.size > 0) {
                 event.isCanceled = true
+                Minecraft.getInstance().gui.chat.addRecentChat(event.originalMessage)
                 UltraDex.COMMANDS_DISPATCHER.execute(parse)
             }
-        } catch (_: CommandSyntaxException) {
+        } catch (e: CommandSyntaxException) {
+            UltraDex.LOGGER.error("error running command ${event.originalMessage}: $e")
+            source.sendFailure(TranslationTextComponent(e.message ?: "error running command"))
         }
     }
 
--- src/main/kotlin/com/minmax/ultradex/journeymap/LocateBiomeCommand.kt
+++ src/main/kotlin/com/minmax/ultradex/journeymap/LocateBiomeCommand.kt
@@ -1,95 +1,86 @@
 package com.minmax.ultradex.journeymap
 
 import com.minmax.ultradex.util.BiomeUtils.getDisplayName
-import com.mojang.brigadier.context.CommandContext
+import com.mojang.brigadier.builder.LiteralArgumentBuilder
 import com.mojang.brigadier.exceptions.DynamicCommandExceptionType
 import journeymap.client.data.DataCache
 import journeymap.client.io.FileHandler
 import journeymap.client.model.MapType
 import journeymap.client.model.RegionCoord
+import journeymap.common.helper.BiomeHelper
 import journeymap.common.nbt.RegionDataStorageHandler
 import net.minecraft.client.Minecraft
 import net.minecraft.command.CommandSource
 import net.minecraft.command.Commands
 import net.minecraft.util.ResourceLocation
 import net.minecraft.util.math.BlockPos
-import net.minecraft.util.text.StringTextComponent
 import net.minecraft.util.text.TextComponentUtils
 import net.minecraft.util.text.TranslationTextComponent
-import net.minecraft.world.biome.Biome
+import net.minecraft.world.biome.Biomes
 import net.minecraftforge.registries.ForgeRegistries
+import kotlin.math.floor
+import kotlin.math.sqrt
 
 
 object LocateBiomeCommand {
-//    var LOCATE_BIOME_COMMAND: LiteralArgumentBuilder<CommandSource> = Commands.literal("locatebiome")
-//        .then(Commands.argument("biome", ResourceLocationArgument.id()))
-//        .executes { ctx -> execute(ctx.source, ctx.getArgument("biome", ResourceLocation::class.java)) }
-
+    val CMD: LiteralArgumentBuilder<CommandSource> = Commands.literal("test")
+        .requires { cs -> cs.hasPermission(0) }
+        .executes { ctx -> execute(ctx.source, Biomes.DESERT.location()) }
 
     private fun execute(src: CommandSource, biomeRl: ResourceLocation): Int {
         val biome = ForgeRegistries.BIOMES.getValue(biomeRl) ?: throw INVALID_EXCEPTION.create(biomeRl)
         val biomeName = biome.getDisplayName()
 
-        val biomePos =
-            locateBiome(biome, BlockPos(src.position), MAX_DISTANCE, STEPS)
-                ?: throw NOT_FOUND_EXCEPTION.create(biomeName)
+        val biomePos = locateBiome(biomeRl, BlockPos(src.position), MAX_DISTANCE, STEPS)
+            ?: throw NOT_FOUND_EXCEPTION.create(biomeName)
+        val distance = floor(BlockPos(src.position).distance(biomePos)).toInt()
         src.sendSuccess(
             TranslationTextComponent(
-                "commands.locate.success", biomeName,
+                "commands.locate.success",
+                biomeName,
                 TextComponentUtils.wrapInSquareBrackets(
                     TranslationTextComponent("chat.coordinates", biomePos.x, "~", biomePos.z)
-                )
+                ),
+                distance
             ), false
         )
 
-        return 1
-    }
-
-    fun run(ctx: CommandContext<CommandSource>): Int {
-        ctx.source.sendSuccess(StringTextComponent("Hello World!"), false)
-        return 0
+        return distance
     }
 
 
-    val CMD = Commands.literal("test")
-        .requires { cs -> cs.hasPermission(0) }
-        .executes { ctx -> run(ctx) }
-
-//        private val CMD = LocateBiomeCommand()
-
-    private fun locateBiome(targetBiome: Biome, pos: BlockPos, maxDistance: Int, step: Int): BlockPos? {
+    private fun locateBiome(targetBiome: ResourceLocation, pos: BlockPos, maxDistance: Int, step: Int): BlockPos? {
         val player = DataCache.getPlayer()
         val mapType = MapType.biome(player)
-        for (dx in -maxDistance..maxDistance step step) {
-            for (dz in -maxDistance..maxDistance step step) {
-                val newPos = BlockPos(pos.x + dx, pos.y, pos.z + dz)
-                val regionCoord = RegionCoord.fromChunkPos(
-                    FileHandler.getJMWorldDir(Minecraft.getInstance()),
-                    mapType,
-                    newPos.x shr 4,
-                    newPos.z shr 4
-                )
-                val regionData = RegionDataStorageHandler.getInstance()
-                    .getRegionDataAsyncNoCache(regionCoord, mapType)
-                val biome = regionData.getBiome(newPos)
-                if (biome == targetBiome) {
-                    return newPos
-                }
-            }
-        }
-        return null
+        return (-maxDistance..maxDistance step step)
+            .zip(-maxDistance..maxDistance step step)
+            .map { BlockPos(pos.x + it.first, pos.y, pos.z + it.second) }
+            .find { getJMBiome(it, mapType) == targetBiome }
     }
 
-    private const val MAX_DISTANCE = 64000
-    private const val STEPS = 8
+    private fun getJMBiome(newPos: BlockPos, mapType: MapType): ResourceLocation? {
+        val regionPos = RegionCoord.fromChunkPos(
+            FileHandler.getJMWorldDir(Minecraft.getInstance()),
+            mapType,
+            newPos.x shr 4,
+            newPos.z shr 4
+        )
+        val regionData = RegionDataStorageHandler.getInstance().getRegionDataAsyncNoCache(regionPos, mapType)
+        val biome = regionData.getBiome(newPos) ?: return null
+        return BiomeHelper.getBiomeResource(biome)
+    }
+
+    private fun BlockPos.distance(pos: BlockPos): Double {
+        val dx = this.x - pos.x
+        val dz = this.z - pos.z
+        return sqrt((dx * dx + dz * dz).toDouble())
+    }
+
+    private const val MAX_DISTANCE = 128
+    private const val STEPS = 64
 
     private val INVALID_EXCEPTION =
         DynamicCommandExceptionType { TranslationTextComponent("There is no biome named %s", it) }
     private val NOT_FOUND_EXCEPTION =
-        DynamicCommandExceptionType {
-            TranslationTextComponent(
-                "Could not find a %s within reasonable distance",
-                it
-            )
-        }
+        DynamicCommandExceptionType { TranslationTextComponent("Could not find a %s within distance", it) }
 }
